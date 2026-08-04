391d4639b8c9760f85d6d798bc4db70787bcfe45
hassan2314 <hassan87691@gmail.com>
2026-08-04 09:27:19 +0500

    define table

--- yoututbe_db_conn.session.sql
+++ yoututbe_db_conn.session.sql
@@ -30,3 +30,130 @@ INSERT INTO employees VALUES
 (109, 'Zain', 55000, 2, 103),
 (110, 'Noor', 60000, 1, 101);
 
+-- Level 1
+
+SELECT employees.employee_name
+ FROM employees
+ WHERE employees.salary > 
+ (SELECT AVG(salary) from employees);
+
+ SELECT employees.employee_name
+ FROM employees
+ WHERE salary = (SELECT max(salary) from employees);
+
+Select employee_name, salary
+from employees
+where salary < (Select min(salary) from employees where department_id = (select department_id from departments where department_name='IT'));
+ 
+Select employee_name
+From employees
+where department_id = (Select department_id from employees where employee_name='Sara' );
+
+select employee_name
+From employees
+where manager_id = (select manager_id from employees where employee_name='Usman' );
+
+-- Level 2
+
+Select employee_name
+From employees
+where department_id=
+In
+(select department_id 
+from departments 
+where department_name 
+like('M%'));
+
+Select employee_name
+From employeides
+where department_id in
+(
+    select department_id 
+    From employees
+    group by department_id
+    having count(*)>1
+);
+
+Select employee_name
+From employees
+where employee_id
+In (
+    select manager_id
+    from employees 
+    Where manager_id is not null
+);
+
+-- Level 3
+
+SELECT employee_name, salary
+FROM employees
+WHERE salary > ALL (
+    SELECT salary
+    FROM employees
+    WHERE department_id = 1
+);
+
+SELECT employee_name 
+FROM employees
+where salary > ANY (
+    select salary
+    FROM employees
+    WHERE department_id=3
+);
+
+--Level 4
+
+SELECT department_name
+FROM departments d
+WHERE EXISTS (
+    SELECT 1
+    FROM employees e
+    WHERE e.department_id = d.department_id
+);
+
+SELECT employee_name
+FROM employees e1
+WHERE EXISTS (
+    SELECT 1
+    FROM employees e2
+    WHERE e2.manager_id = e1.employee_id
+);
+
+--Level 5
+
+select e.employee_name, d.department_name
+from employees e
+join departments d
+on d.department_id =e.department_id
+where salary >
+(
+    select AVG(salary)
+     as avg_sal
+    from employees e2
+    where e2.department_id =e.department_id
+);
+
+
+select e.employee_name, d.department_name
+from employees e
+join departments d 
+on e.department_id=d.department_id
+where salary = (
+select max(salary)
+from employees e2
+where e2.department_id=e.department_id
+);
+
+select e.employee_name, d.department_name
+from employees e
+join departments d 
+on e.department_id=d.department_id
+where salary = (
+select min(salary)
+from employees e2
+where e2.department_id=e.department_id
+);
+
+
+
+
